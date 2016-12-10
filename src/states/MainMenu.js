6c8a3fdd1ba396339f8b9ad627e773cb15ffa8a7
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-10 19:29:37 +0100

    add link to resource + remove camera movement

--- src/states/MainMenu.js
+++ src/states/MainMenu.js
@@ -1,5 +1,7 @@
 import { LD, Wall } from "SpriteConstants";
 import { CameraVelocity, Bounds } from "Constants";
+
+import Player from "objects/Character";
 import Room from "objects/Room";
 
 class MainMenu extends Phaser.State {
@@ -7,8 +9,11 @@ class MainMenu extends Phaser.State {
   create() {
     this.game.world.setBounds(0, 0, Bounds, Bounds);
     this.game.add.sprite(50,100, LD);
-    this.room = new Room(this.game)
+    this.room = new Room(this.game);
     this.game.add.existing(this.room);
+
+    this.player = new Player(this.game,45,78);
+    this.game.add.existing(this.player);
     this.cursors = this.game.input.keyboard.createCursorKeys();
     //this.room.createRandomLine(32, 128, 32 + 9 * 32, 128, 3);
     //this.room.createLine(20, 400, 400, 400);
@@ -16,6 +21,9 @@ class MainMenu extends Phaser.State {
   }
 
   update() {
+  }
+
+  moveCamera() {
     if (this.cursors.up.isDown)
     {
       this.game.camera.y -= CameraVelocity;
@@ -38,6 +46,7 @@ class MainMenu extends Phaser.State {
   preload() {
     this.game.load.image(LD, "res/LD.png");
     this.game.load.image(Wall, "res/1.png");
+    this.game.load.image(Player, "res/player.png");
   }
 
 }
