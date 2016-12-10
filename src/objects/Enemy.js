262951455e5dba216d877070245a8450121acde4
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-10 21:02:49 +0100

    add enemy

---
+++ src/objects/Enemy.js
@@ -0,0 +1,26 @@
+import { SpriteEnemy } from "../SpriteConstants";
+
+class Enemy extends Phaser.Sprite {
+
+  constructor(game, x, y, key, frame) {
+    super(game, x, y, SpriteEnemy, frame);
+    //Enable physics on the player
+    game.physics.arcade.enable(this);
+    this.body.bounce.x = this.body.bounce.y = 0;
+    this.cursor = game.input.keyboard.createCursorKeys();
+    //this.body.gravity.y = 500;
+    this.body.mass = 10;
+    this.direction = 1;
+  }
+
+  update(player) {
+  }
+
+  isDeath() {
+    if (!this.body) {
+      return false;
+    }
+  }
+}
+
+export default Enemy;
