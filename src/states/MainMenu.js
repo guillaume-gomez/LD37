7b76bffe0f67b8fbbd2a12740e502d52dd609d8c
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-10 20:45:11 +0100

    follow character

--- src/states/MainMenu.js
+++ src/states/MainMenu.js
@@ -1,8 +1,10 @@
-import { LD, Wall } from "SpriteConstants";
+import { LD, Wall, SpriteConstants } from "SpriteConstants";
 import { CameraVelocity, Bounds } from "Constants";
 
 import Player from "objects/Character";
 import Room from "objects/Room";
+
+const needCamera = false;
 
 class MainMenu extends Phaser.State {
 
@@ -12,41 +14,51 @@ class MainMenu extends Phaser.State {
     this.room = new Room(this.game);
     this.game.add.existing(this.room);
 
-    this.player = new Player(this.game,45,78);
-    this.game.add.existing(this.player);
-    this.cursors = this.game.input.keyboard.createCursorKeys();
+    this.hero = new Player(this.game,150,100);
+    this.game.add.existing(this.hero);
+    if(needCamera) {
+      this.cursors = this.game.input.keyboard.createCursorKeys();
+    }
     //this.room.createRandomLine(32, 128, 32 + 9 * 32, 128, 3);
     //this.room.createLine(20, 400, 400, 400);
+    this.camera.follow(this.hero)
     this.room.createRandomSquare(32,0,18,1, +2, 1 );
   }
 
   update() {
+    this.game.physics.arcade.collide(this.hero, this.room);
   }
 
   moveCamera() {
-    if (this.cursors.up.isDown)
-    {
-      this.game.camera.y -= CameraVelocity;
-    }
-    else if (this.cursors.down.isDown)
-    {
-      this.game.camera.y += CameraVelocity;
-    }
+    if(needCamera) {
+      if (this.cursors.up.isDown)
+      {
+        this.game.camera.y -= CameraVelocity;
+      }
+      else if (this.cursors.down.isDown)
+      {
+        this.game.camera.y += CameraVelocity;
+      }
 
-    if (this.cursors.left.isDown)
-    {
-      this.game.camera.x -= CameraVelocity;
-    }
-    else if (this.cursors.right.isDown)
-    {
-      this.game.camera.x += CameraVelocity;
+      if (this.cursors.left.isDown)
+      {
+        this.game.camera.x -= CameraVelocity;
+      }
+      else if (this.cursors.right.isDown)
+      {
+        this.game.camera.x += CameraVelocity;
+      }
     }
   }
 
   preload() {
     this.game.load.image(LD, "res/LD.png");
     this.game.load.image(Wall, "res/1.png");
-    this.game.load.image(Player, "res/player.png");
+    this.game.load.image(SpriteConstants, "res/player.png");
+  }
+
+  render() {
+     //this.game.debug.spriteInfo(this.hero, 32, 32);
   }
 
 }
