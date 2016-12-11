04267b0c51d0251efb09dd16cb74c4720edf338e
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-11 19:46:30 +0100

    add menu

---
+++ src/states/Menu.js
@@ -0,0 +1,23 @@
+
+class Menu extends Phaser.State {
+
+  create() {
+    this.game.add.sprite(175,100, "LD");
+    this.game.add.text(230, 230, "Press enter to start", { font: "bold 34px Arial", fill: "#fff" });
+    this.game.add.text(280, 350, "Thanks for playing ! :)", { font: "bold 19px Arial", fill: "#fff" })
+    this.game.add.text(200, 400, "Compo during LD37 in December 2016", { font: "bold 19px Arial", fill: "#fff" });
+    this.enterButton = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+  }
+
+  preload() {
+    this.game.load.image("LD", "res/LD.png");
+  }
+
+  update() {
+    if(this.enterButton.isDown) {
+    }
+  }
+
+}
+
+export default Menu;
