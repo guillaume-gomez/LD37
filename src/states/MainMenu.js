56f41d6c3e76edc8e5950f23f2b8e87f6d3ced2f
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-11 19:38:56 +0100

    change color of debug...not very important

--- src/states/MainMenu.js
+++ src/states/MainMenu.js
@@ -165,7 +165,7 @@ class MainMenu extends Phaser.State {
   }
 
   render() {
-    this.game.debug.text(Bounds, 2, 14, "#00ff00");
+    this.game.debug.text(Bounds, 2, 14, "#ff0000");
     //this.game.debug.spriteInfo(this.hero, 32, 32);
   }
 
