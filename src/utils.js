7778c8d2f04576edcba803a61712c4517099d972
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-01-16 17:58:16 +0100

    create new util function to detect if a pad is connected

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
 export function getRandomArbitrary(min, max) {
     return Math.trunc(Math.random() * (max - min) + min);
 }
+
+export function hasGamepad(game) {
+  return game.input.gamepad.supported && game.input.gamepad.active && game.input.gamepad.pad1.connected;
+}
