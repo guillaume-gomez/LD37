0bffe405f49d4eb755393d9b4e27c4b4120832f2
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-09-10 20:01:16 +0200

    update the filter

--- src/objects/BackgroundShader.js
+++ src/objects/BackgroundShader.js
@@ -11,19 +11,24 @@ class BackgroundShader extends Phaser.Sprite {
         "uniform vec2     resolution;",
 
         "void main() {",
-            "vec3 h_color_top = vec3(1.5, 0.5, 10.0) * clamp(sin(time), 0.2, 1.0);",
-            "vec3 h_color_bottom = vec3(0, 0, 1) * clamp(sin(time), 0.2, 1.0);",
+            "vec3 h_color_top = vec3(1.5, 0.5, 10.0) * clamp(sin(time), 0.5, 1.0);",
+            "vec3 h_color_bottom = vec3(0, 0, 1) * clamp(sin(time), 0.5, 1.0);",
             "gl_FragColor = vec4(h_color_top * (gl_FragCoord.y / resolution.y), 1) + vec4(h_color_bottom * (1.4 - (gl_FragCoord.y / resolution.y)), 1);",
         "}"
 
     ];
 
-    const filter = new Phaser.Filter(game, null, fragmentSrc);
-    filter.setResolution(ScreenWidth, ScreenHeight);
     super(game, 0, 0);
+    this.filter = new Phaser.Filter(game, null, fragmentSrc);
+    this.filter.setResolution(ScreenWidth, ScreenHeight);
+
     this.width = ScreenWidth;
     this.height = ScreenHeight;
-    this.filters = [ filter ];
+    this.filters = [ this.filter ];
+  }
+
+  update() {
+     this.filter.update();
   }
 
 }
