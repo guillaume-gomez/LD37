574f9b834ec8c8fa43fe53b5f6a6c1ea5601abbd
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-09-11 20:30:36 +0200

    add second filter

---
+++ src/objects/DistortionFilter.js
@@ -0,0 +1,28 @@
+import { ScreenWidth, ScreenHeight } from "../Constants";
+
+class DistortionFilter extends Phaser.Filter {
+
+  constructor(game) {
+     const fragmentSrc = [
+      "precision mediump float;",
+        "uniform sampler2D Texture0;",
+        "uniform sampler2D Texture1;",
+        "varying vec2 vTextureCoord;",
+
+        "void main()",
+        "{",
+        "  vec2 pos = vTextureCoord;",
+        "  float distortion = 0.07;",
+        "  pos -= vec2(0.5, 0.5);",
+        "  pos *= vec2(pow(length(pos), distortion));",
+        "  pos += vec2(0.5, 0.5);",
+        "  gl_FragColor = texture2D(Texture0, pos);",
+        "}"
+    ];
+    super(game, null, fragmentSrc);
+    this.setResolution(ScreenWidth, ScreenHeight);
+  }
+
+}
+
+export default DistortionFilter;
